6db226eb9a5f69b2f8b7038c948baf2e47140383
mateuszbabski <babskimateusz@gmail.com>
2024-11-17 17:04:17 +0100

    better solution

--- LeetCode/C#/roman_to_integer.cs
+++ LeetCode/C#/roman_to_integer.cs
@@ -2,23 +2,40 @@ public class Solution
 {
     public int RomanToInt(string s)
     {
-        Dictionary<char, int> romanMap = new Dictionary<char, int>
-         {
-            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
-        };
         int result = 0;
+
         for (int i = 0; i < s.Length - 1; i++)
         {
-            if (romanMap[s[i]] >= romanMap[s[i + 1]])
+            int currentValue = GetValue(s[i]);
+            int nextValue = GetValue(s[i + 1]);
+
+            if (currentValue >= nextValue)
             {
-                result += romanMap[s[i]];
+                result += currentValue;
             }
             else
             {
-                result -= romanMap[s[i]];
+                result -= currentValue;
             }
         }
-        result += romanMap[s[s.Length - 1]];
+
+        result += GetValue(s[s.Length - 1]);
+
         return result;
+    }
+
+    private int GetValue(char c)
+    {
+        switch (c)
+        {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
     }
 }
